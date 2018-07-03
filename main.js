55c4b39850d4fca825fea4c6f119e5f27023f775
Joseph Lander <josephclander@gmail.com>
2018-07-03 08:45:17 +0100

    added a fade style to bottom of posts

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(window).ready(function(){
 	$('.more').click(function(){
    
   		$(this).parents('.post').toggleClass('reveal');
-
+  		$(this).siblings('.overlay').toggleClass('fade');
   	})
 
 })
