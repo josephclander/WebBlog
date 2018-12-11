a024ab94d94f2db2174db714df0f617c2ff92ea0
Joseph Lander <josephclander@gmail.com>
2018-12-11 17:43:50 -0500

    js added to update the github info

--- main.js
+++ main.js
@@ -1,71 +1,89 @@
-$(window).ready(function(){
-
-
+$(window).ready(function() {
 /*****************************
+Github results box API
+*****************************/
+
+  fetch("https://api.github.com/users/josephclander")
+    .then(response => response.json())
+    .then(myJson => {
+	  console.log(myJson);
+	  let repos = document.querySelector('#js-repositories');
+		repos.innerHTML = myJson.public_repos;
+	});
+	
+	fetch("https://api.github.com/repos/josephclander/webBlog/commits?per_page=100")
+    .then(response => response.json())
+    .then(myJson => {
+	  console.log(myJson.length);
+		let commits = document.querySelector('#js-commits');
+		commits.innerHTML = myJson.length;
+    });
+
+  /*****************************
 Expand Articles
 *****************************/
 
-	$('.more').click(function(event){
-   		event.preventDefault();
-  		$(this).parents('.post').toggleClass('reveal');
-  		$(this).siblings('.overlay').toggleClass('fade');
-  	})
-	
-/*****************************
+  $(".more").click(function(event) {
+    event.preventDefault();
+    $(this)
+      .parents(".post")
+      .toggleClass("reveal");
+    $(this)
+      .siblings(".overlay")
+      .toggleClass("fade");
+  });
+
+  /*****************************
 Toggle Filter List
 *****************************/
 
-$('li.topics').click(function(event){
+  $("li.topics").click(function(event) {
+    event.preventDefault();
 
-	event.preventDefault();
-	
-	var x = document.getElementById("buttons");
+    var x = document.getElementById("buttons");
 
-	if (x.style.display === "inline-block") {
-		x.style.display = "none";
-	} else {
-		x.style.display = "inline-block";
-	}
-})
+    if (x.style.display === "inline-block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "inline-block";
+    }
+  });
 
-
-/*****************************
+  /*****************************
 Filter Topics
 *****************************/
 
-	var selection;
-	$('a.filter').click(function(event){
-		
-		event.preventDefault();
+  var selection;
+  $("a.filter").click(function(event) {
+    event.preventDefault();
 
+    $(this).addClass("active");
+    $("a.filter")
+      .not(this)
+      .removeClass("active");
 
-		$(this).addClass('active');
-		$('a.filter').not(this).removeClass('active');
+    selection = $(".active")
+      .text()
+      .toLowerCase();
 
-		selection = $('.active').text().toLowerCase();
+    if (selection === "all") {
+      $("article").show();
+      // notice this is element called "article" and not class name
+    }
 
-		if (selection === "all") {
+    if (selection !== "all") {
+      var toShow = document.getElementsByClassName(selection);
+      for (let i = 0; i < toShow.length; i++) {
+        toShow[i].setAttribute("style", "display: block");
+      }
 
-			$("article").show();
-			// notice this is element called "article" and not class name
-		}
+      var selectionClass = "." + selection;
+      // this step added to get the class of the name selected
+      var toHide = $("article").not(selectionClass);
 
-		if (selection !== "all") {
-
-			var toShow = document.getElementsByClassName(selection);
-			for (let i = 0; i < toShow.length; i++) {
-				toShow[i].setAttribute("style", "display: block");
-			}
-
-			var selectionClass = "." + selection;
-			// this step added to get the class of the name selected
-			var toHide = $('article').not(selectionClass);
-		
-			for (let i = 0; i < toHide.length; i++) {
-				toHide[i].setAttribute("style", "display: none");
-			}
-			
-		}
-	})
-
-})
+      for (let i = 0; i < toHide.length; i++) {
+        toHide[i].setAttribute("style", "display: none");
+      }
+    }
+  });
+});
